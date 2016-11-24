26ebf82704c8f8c321e423d957db37d6ec3d29b6
Matheson Mawhinney <mmawhinn@sfu.ca>
2016-11-23 17:00:34 -0800

    histogram function now parallelized

--- histogram-equalization.cu
+++ histogram-equalization.cu
@@ -3,22 +3,23 @@
 #include <stdlib.h>
 #include "histogram-equalization.h"
 
-void print_hist(int *hist) {
-    for(int i = 0; i < 256; i++) {
-        printf("hist[%d] = %d\n", i, hist[i]);
-    }
-}
+__global__ void histogram_gpu(int * hist_out, unsigned char * img_in, int *img_size){
+    extern __shared__ int temp[];
 
-__global__ void histogram_gpu(int * hist_out, unsigned char * img_in, int *img_size, int *nbr_bin){
-    printf("executed...\n");
-    int i;
-    for ( i = 0; i < *nbr_bin; i ++){
-        hist_out[i] = 0;
+    temp[threadIdx.x] = 0;
+
+    __syncthreads();
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int offset = blockDim.x * gridDim.x;
+    while(i < *img_size) {
+        atomicAdd(&(temp[img_in[i]]), 1);
+        i += offset;
     }
 
-    for ( i = 0; i < *img_size; i ++){
-        hist_out[img_in[i]] ++;
-    }
+    __syncthreads();
+
+    atomicAdd(&(hist_out[threadIdx.x]), temp[threadIdx.x]);
 }
 
 void histogram_equalization_gpu(unsigned char * img_out, unsigned char * img_in, 
@@ -59,42 +60,30 @@ void histogram_equalization_gpu(unsigned char * img_out, unsigned char * img_in,
 void create_histogram_gpu(int *hist, unsigned char* img_in, int img_size, int nbr_bin, 
                         unsigned char *img_out)
 {
-
     int *hist_d;
     unsigned char *img_in_d;
     int *img_size_d;
-    int *nbr_bin_d;
 
     cudaMalloc((void**)&hist_d, sizeof(int)*256);
     cudaMalloc((void**)&img_in_d, sizeof(unsigned char) * img_size);
     cudaMalloc((void**)&img_size_d, sizeof(int));
-    cudaMalloc((void**)&nbr_bin_d, sizeof(int));
 
-    cudaMemcpy(hist_d, hist, sizeof(int)*256, cudaMemcpyHostToDevice);
     cudaMemcpy(img_in_d, img_in, sizeof(unsigned char) * img_size, cudaMemcpyHostToDevice);
     cudaMemcpy(img_size_d, &img_size, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(nbr_bin_d, &nbr_bin, sizeof(int), cudaMemcpyHostToDevice);
 
-    printf("%s\n", cudaGetErrorString(cudaGetLastError()));
+    cudaMemset(hist_d, 0, sizeof(int) * 256);
 
-    histogram_gpu<<<1,1>>>(hist_d, img_in_d, img_size_d, nbr_bin_d);
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    int blocks = prop.multiProcessorCount;
 
-    printf("%s\n", cudaGetErrorString(cudaGetLastError()));
-
+    histogram_gpu<<<blocks * 2 ,nbr_bin, nbr_bin * sizeof(int)>>>(hist_d, img_in_d, img_size_d);
 
     cudaMemcpy(hist, hist_d, sizeof(int)*256, cudaMemcpyDeviceToHost);
-
-    print_hist(hist);
-
-    printf("%s\n", cudaGetErrorString(cudaGetLastError()));
 
     cudaFree(hist_d);
     cudaFree(img_in_d);
     cudaFree(img_size_d);
-    cudaFree(nbr_bin_d);
-
-    printf("%s\n", cudaGetErrorString(cudaGetLastError()));
-
 
     histogram_equalization_gpu(img_out, img_in, hist, img_size, nbr_bin);
 }
